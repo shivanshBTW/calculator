ea13d32788c85c2cde098d0e39fafae92e099379
Shivansh Tyagi <stshivansh99@gmail.com>
2018-06-17 20:20:12 +0530

    4

--- js/controller.js
+++ js/controller.js
@@ -1,33 +1,38 @@
-window.addEventListener("load",init);
+window.addEventListener("load", init);
 
-function init(){
+function init() {
     bindEvents();
 }
 
-function bindEvents(){
+function bindEvents() {
     var buttons = document.querySelectorAll('.operationButtons button');
-    for(button of buttons){
-        button.addEventListener('click',compute)
+    for (button of buttons) {
+        button.addEventListener('click', compute)
     }
-    document.querySelector('#clearAll').addEventListener('click',clearAll);
+    document.querySelector('#clearAll').addEventListener('click', clearAll);
 }
 
-function compute(event){
+function compute(event) {
     var operation = event.srcElement.id;
     // console.log(operation)
     var firstNumber = parseInt(document.querySelector('#firstNumber').value);
     var secondNumber = parseInt(document.querySelector('#secondNumber').value);
-    calcOperations.takeNumbers(firstNumber,secondNumber);
+    calcOperations.takeNumbers(firstNumber, secondNumber);
     calcOperations.result = calcOperations[operation]();
     //console.log(result);
     printResult();
 }
 
-function printResult(){
-    document.querySelector('#result').innerText = calcOperations.result;
+function printResult() {
+
+    if (calcOperations.result == 'Infinity') {
+        document.querySelector('#result').innerText = 'Divide By Zero Exception';
+
+    } else if (calcOperations.result)
+        document.querySelector('#result').innerText = calcOperations.result;
 }
 
-function clearAll(){
+function clearAll() {
     document.querySelector('#firstNumber').value = "";
     document.querySelector('#secondNumber').value = "";
     document.querySelector('#result').innerText = "";
